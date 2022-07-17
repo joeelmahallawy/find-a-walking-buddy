461a3d24a14392b320046af4e85db8c0c82cd4d4
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-07-16 21:58:30 -0700

    added html meta titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import logo from "../assets/fawb-trans.png";
+import React from "react";
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
@@ -13,6 +16,8 @@ const App = (props: AppProps) => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+
+        <link rel="shortcut icon" href={logo.src} sizes="16x16"></link>
       </Head>
 
       <MantineProvider
